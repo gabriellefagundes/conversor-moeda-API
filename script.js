c70feb84ba5c8eb1a636be829a478ec885030d4f
Larissa Gabrielle Fagundes Andrade <larissa.gabrielle@alunoescolassociais.org.br>
2024-11-01 08:43:50 -0300

    Começamos a criação do js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-https://v6.exchangerate-api.com/v6/235d2d5a6de0c2230119068c/latest/USD
+const apiKey = '235d2d5a6de0c2230119068c'
+const apiURL = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/`;
+
+//Função para consulta à taxa de câmbio via API
+async function getExchangeRate(daMoeda, paraMoeda){
+    try{
+        const response = await fetch(`${apiURL}${daMoeda}`);
+        const data = response.json();
+
+        if(data.result === "succsess"){
+            return data.conversion_rates[paraMoeda];
+        }else{
+            throw new Error('Erro ao buscar a taxa de câmbio.');
+        }
+    }catch(error){
+        console.error("Erro:", error);
+        return null;
+    }
+}
